1ddc619a08e92db6ed8ce96624d3433d1769bf1f
admin <rsk.mva@gmail.com>
2019-12-05 20:47:13 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] names = {"John", "Mark", "Michal"};
+        String [] names2 = {names[0], names [1], names[2],"Tom"};
+
+        for (String name : names2) {
+
+            switch (name) {
+
+                case "John":
+                    System.out.println("Good Morning "  + name);
+                    break;
+
+                case "Mark":
+                    System.out.println("Good day " +  name);
+                    break;
+
+                case "Michal":
+                    System.out.println("Good evening "+ name);
+                    break;
+                case "Tom":
+                    System.out.println("Good bye "  + name);
+
+
+            }
+        }
+    }
+}
